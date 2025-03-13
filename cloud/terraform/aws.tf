e3ef81d88c6b4e1b2e4560caf358351087e0bdb3
August Feng <augustfeng@Mac.localdomain>
2025-03-12 22:50:14 -0400

    associate public ip address

--- cloud/terraform/aws.tf
+++ cloud/terraform/aws.tf
@@ -395,8 +395,10 @@ resource "aws_instance" "simple-login" {
   instance_type = "t4g.micro"
   key_name      = aws_key_pair.augustfeng.key_name
 
-  vpc_security_group_ids = [aws_security_group.simple-login.id]
-  subnet_id              = aws_subnet.compute-1a.id
+
+  vpc_security_group_ids      = [aws_security_group.simple-login.id]
+  subnet_id                   = aws_subnet.compute-1a.id
+  associate_public_ip_address = true
 
   instance_market_options {
     market_type = "spot"
