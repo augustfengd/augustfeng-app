25eeb7166cfa800c4fd7b323537eeaf3e7234679
August Feng <augustfengd@gmail.com>
2025-03-19 22:48:34 -0400

    add us-east-1

--- cloud/terraform/aws.tf
+++ cloud/terraform/aws.tf
@@ -522,7 +522,7 @@ resource "aws_secretsmanager_secret_version" "simple-login" {
 
 resource "terraform_data" "aws-sesv2-put-account-details-production-access-enabled" {
   provisioner "local-exec" {
-    command = "aws sesv2 put-account-details --production-access-enabled --mail-type TRANSACTIONAL --website-url https://augustfeng.email --additional-contact-email-addresses augustfeng@augustfeng.email --contact-language EN"
+    command = "aws --region us-east-1 sesv2 put-account-details --production-access-enabled --mail-type TRANSACTIONAL --website-url https://augustfeng.email --additional-contact-email-addresses augustfeng@augustfeng.email --contact-language EN"
   }
 }
 
