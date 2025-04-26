9ade1a12cbbf1d28ffa5337353cea3a5f9d1505a
August Feng <augustfengd@gmail.com>
2025-04-26 12:49:59 +0100

    remove uneeded sg

--- cloud/terraform/socks/aws.tf
+++ cloud/terraform/socks/aws.tf
@@ -51,11 +51,3 @@ resource "aws_vpc_security_group_ingress_rule" "socks-ssh" {
   to_port           = 22
   ip_protocol       = "tcp"
 }
-
-resource "aws_vpc_security_group_ingress_rule" "socks-socks" {
-  security_group_id = aws_security_group.socks.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 1080
-  to_port           = 1080
-  ip_protocol       = "tcp"
-}
